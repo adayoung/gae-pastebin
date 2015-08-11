7bfee21be8dc00e4f80d2fa62c8c40d7c059cfed
Ada Young <adayoung@users.noreply.github.com>
2015-08-11 15:18:39 +0530

    I wanna see teh travis thingie!

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # gae-pastebin
+
+[![Build Status](https://travis-ci.org/adayoung/gae-pastebin.svg?branch=yay_testing)](https://travis-ci.org/adayoung/gae-pastebin)
+
 It's a pastebin that runs on Google App Engine! There is a running instance here: https://ada-young.appspot.com/pastebin ..!
---
+++ tests/tests.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+import unittest
+import os
+
+class CheckEnv(unittest.TestCase):
+
+	def setUp(self):
+		username = os.environ["SAUCE_USERNAME"]
+		access_key = os.environ["SAUCE_ACCESS_KEY"]
+
+		capabilities = {
+			'browserName': 'firefox',
+			'platform': 'Linux',
+			'version': 'beta',
+		}
+
+		capabilities["build"] = os.environ["TRAVIS_BUILD_NUMBER"]
+		capabilities["tags"] = [os.environ["TRAVIS_PYTHON_VERSION"], "CI"]
+		capabilities["tunnel-identifier"] = os.environ["TRAVIS_JOB_NUMBER"]
+
+		hub_url = "%s:%s@localhost:4445" % (username, access_key)
+		self.browser = webdriver.Remote(desired_capabilities=capabilities, command_executor="http://%s/wd/hub" % hub_url)
+		self.browser.implicitly_wait(3)
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_selenium(self):
+		self.browser.get('http://localhost:8080/')
+		self.assertIn('Bad Gateway', self.browser.title)
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
