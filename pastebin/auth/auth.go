20b2afb247300316b174d37400bf01d7b371b5ec
Ada Young <adayoung@users.noreply.github.com>
2016-10-07 10:01:44 +0000

    Let's confirm the currently logged in user before going ahead with requesting Google Drive authorization :o

--- pastebin/auth/auth.go
+++ pastebin/auth/auth.go
@@ -2,14 +2,17 @@ package auth
 
 import (
 	// Go Builtin Packages
+	"html/template"
 	"log"
 	"net/http"
+	"strings"
 
 	// Google Appengine Packages
 	"appengine"
 	"appengine/user"
 
 	// The Gorilla Web Toolkit
+	"github.com/gorilla/csrf"
 	"github.com/gorilla/mux"
 
 	// Local Packages
@@ -20,7 +23,7 @@ var Router *mux.Router
 
 func init() {
 	Router = mux.NewRouter()
-	Router.HandleFunc("/pastebin/auth/login", utils.ExtraSugar(login)).Methods("GET").Name("login")
+	Router.HandleFunc("/pastebin/auth/login", utils.ExtraSugar(login)).Methods("GET", "POST").Name("login")
 	Router.HandleFunc("/pastebin/auth/logout", utils.ExtraSugar(logout)).Methods("GET").Name("logout")
 
 	Router.HandleFunc("/pastebin/auth/gdrive", utils.ExtraSugar(auth_gdrive_begin)).Methods("GET").Name("auth_gdrive_begin")
@@ -37,10 +40,22 @@ func login(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if usr := user.Current(c); usr != nil { // already logged in
-		if dest != "/pastebin/login/" {
-			http.Redirect(w, r, dest, http.StatusFound)
-		} else {
+		if r.Method == "GET" {
+			var tmpl = template.Must(template.ParseFiles("templates/base.tmpl","pastebin/templates/auth.tmpl"))
+			if err := tmpl.Execute(w, map[string]interface{}{
+				csrf.TemplateTag: csrf.TemplateField(r),
+				"dest": dest,
+				"user": user.Current(c),
+			}); err != nil {
+				log.Panic(c, err)
+			}
+			return
+		}
+
+		if strings.HasSuffix(dest, "/pastebin/auth/login") {
 			http.Redirect(w, r, "/pastebin/", http.StatusFound)
+		} else {
+			http.Redirect(w, r, dest, http.StatusFound)
 		}
 	} else {
 		if url, err := user.LoginURL(c, dest); err == nil {
@@ -53,7 +68,12 @@ func login(w http.ResponseWriter, r *http.Request) {
 
 func logout(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
-	dest := r.Referer()
+	var dest string
+	if dest = r.FormValue("next"); dest == "" {
+		if dest = r.Referer(); dest == "" {
+			dest = "/pastebin/"
+		}
+	}
 
 	if usr := user.Current(c); usr == nil { // already logged out
 		if dest != "/pastebin/logout/" {
