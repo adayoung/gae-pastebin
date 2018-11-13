d0e53a2bafa8302454b3a324ed4acfb8a53bf55e
Ada Young <adayoung@users.noreply.github.com>
2018-11-13 04:21:31 +0000

    Add cookie and privacy policy, GDPR consent notice.

--- static/js/base.js
+++ static/js/base.js
@@ -17,6 +17,23 @@ $(document).ready(function(){
   })
 });
 
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#edeff5",
+      "text": "#838391"
+    },
+    "button": {
+      "background": "#4b81e8"
+    }
+  },
+  "theme": "edgeless",
+  "content": {
+    "href": "https://ada-young.appspot.com/pastebin/about#PrivacyPolicy"
+  }
+})});
+
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
