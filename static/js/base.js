03d8b32bdc7f620600cbe55ae445d6f47c7a1a37
Ada Young <adayoung@users.noreply.github.com>
2019-09-22 08:30:18 +0100

    Bring back Google Analytics!

--- static/js/base.js
+++ static/js/base.js
@@ -34,14 +34,14 @@ window.cookieconsent.initialise({
   }
 })});
 
-/*
+
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 // FIXME: load GA user id from elsewhere!
-ga('create', 'UA-44889074-1', 'ada-young.appspot.com');
+ga('create', 'UA-44889074-2', 'ada-young.com');
 ga('require', 'displayfeatures');
 ga('send', 'pageview');
-*/
+
