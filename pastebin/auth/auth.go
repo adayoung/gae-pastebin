8f0e2d8c0f32ae82e3b736f223f69604e4b17ddf
Ada Young <adayoung@users.noreply.github.com>
2019-10-04 12:50:42 +0100

    Okay travis ...

--- pastebin/auth/auth.go
+++ pastebin/auth/auth.go
@@ -1,12 +1,6 @@
 package auth
 
 import (
-	// Go Builtin Packages
-	// "html/template"
-	// "net/http"
-	// "net/url"
-	// "strings"
-
 	// The Gorilla Web Toolkit
 	"github.com/gorilla/mux"
 
@@ -15,92 +9,6 @@ import (
 )
 
 func InitRoutes(r *mux.Router) {
-	// r.HandleFunc("/pastebin/auth/login", utils.ExtraSugar(login)).Methods("GET", "POST").Name("login")
-	// r.HandleFunc("/pastebin/auth/logout", utils.ExtraSugar(logout)).Methods("GET").Name("logout")
-
 	r.HandleFunc("/auth/gdrive/start", utils.ExtraSugar(auth_gdrive_start)).Methods("GET").Name("auth_gdrive_start")
 	r.HandleFunc("/auth/gdrive/finish", utils.ExtraSugar(auth_gdrive_finish)).Methods("GET").Name("auth_gdrive_finish")
 }
-
-/*
-func login(w http.ResponseWriter, r *http.Request) {
-	c := appengine.NewContext(r)
-	var dest string
-	if dest = r.FormValue("next"); dest == "" {
-		if dest = r.Referer(); dest == "" {
-			dest = "/pastebin/"
-		}
-	}
-
-	if d, err := url.Parse(dest); err == nil {
-		dest = d.Path
-	} else {
-		dest = "/pastebin/"
-	}
-
-	if usr := user.Current(c); usr != nil { // already logged in
-		if r.Method == "GET" {
-			var tmpl = template.Must(template.ParseFiles("templates/base.tmpl", "pastebin/templates/auth.tmpl"))
-			if err := tmpl.Execute(w, map[string]interface{}{
-				csrf.TemplateTag: csrf.TemplateField(r),
-				"dest":           dest,
-				"user":           user.Current(c),
-			}); err != nil {
-				c.Errorf(err.Error())
-				http.Error(w, "Meep! We were trying to make the 'login' page but something went wrong.", http.StatusInternalServerError)
-				return
-			}
-			return
-		}
-
-		if strings.HasSuffix(dest, "/pastebin/auth/login") {
-			http.Redirect(w, r, "/pastebin/", http.StatusFound)
-			return
-		} else {
-			http.Redirect(w, r, dest, http.StatusFound)
-			return
-		}
-	} else {
-		if url, err := user.LoginURL(c, dest); err == nil {
-			http.Redirect(w, r, url, http.StatusFound)
-			return
-		} else {
-			c.Errorf(err.Error())
-			http.Error(w, "Meep! We were trying to make the 'login' url but something went wrong.", http.StatusInternalServerError)
-		}
-	}
-}
-
-func logout(w http.ResponseWriter, r *http.Request) {
-	c := appengine.NewContext(r)
-	var dest string
-	if dest = r.FormValue("next"); dest == "" {
-		if dest = r.Referer(); dest == "" {
-			dest = "/pastebin/"
-		}
-	}
-
-	if d, err := url.Parse(dest); err == nil {
-		dest = d.Path
-	} else {
-		dest = "/pastebin/"
-	}
-
-	if usr := user.Current(c); usr == nil { // already logged out
-		if dest != "/pastebin/logout/" {
-			http.Redirect(w, r, dest, http.StatusFound)
-			return
-		} else {
-			http.Redirect(w, r, "/pastebin/", http.StatusFound)
-			return
-		}
-	} else {
-		if url, err := user.LogoutURL(c, dest); err == nil {
-			http.Redirect(w, r, url, http.StatusFound)
-		} else {
-			c.Errorf(err.Error())
-			http.Error(w, "Meep! We were trying to make the 'logout' url but something went wrong.", http.StatusInternalServerError)
-		}
-	}
-}
-*/
