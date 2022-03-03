4f615acdb7ecde722b9607b84b8eedeabaa3b213
Ada Young <ada@ada-young.com>
2022-03-03 19:05:11 +0100

    Better back-to-top button!

--- static/js/base.js
+++ static/js/base.js
@@ -2,19 +2,19 @@ $(document).ready(function(){
   /* http://www.developerdrive.com/2013/07/using-jquery-to-add-a-dynamic-back-to-top-floating-button-with-smooth-scroll/ */
   var offset = 220;
   var duration = 500;
-  jQuery(window).scroll(function() {
-      if (jQuery(this).scrollTop() > offset) {
-          jQuery('.back-to-top').fadeIn(duration);
+  $(window).scroll(function() {
+      if ($(this).scrollTop() > offset) {
+          $('.back-to-top').fadeIn(duration);
       } else {
-          jQuery('.back-to-top').fadeOut(duration);
+          $('.back-to-top').fadeOut(duration);
       }
   });
 
-  jQuery('.back-to-top').click(function(event) {
-      event.preventDefault();
-      jQuery('html, body').animate({scrollTop: 0}, duration);
-      return false;
-  })
+  // jQuery('.back-to-top').click(function(event) {
+  //     event.preventDefault();
+  //     jQuery('html, body').animate({scrollTop: 0}, duration);
+  //     return false;
+  // });
 });
 
 window.addEventListener("load", function(){
